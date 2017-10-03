80b111df5b7d28e4e0f0ff4313996b0e1d4bf557
JoseCarlosRN <jose_carlos_rangel@hotmail.com>
2017-10-02 20:51:50 -0400

    Adaptação do layout padrão do perfil do professor

--- numeros/views/layouts/professorMain.php
+++ numeros/views/layouts/professorMain.php
@@ -81,51 +81,10 @@ AppAsset::register($this);
 
     <div class="container theme-showcase" role="main">
         <div class="jumbotron">
-            <div class="row">
-                <div class="col-md-2">
-                    <a href="#">
-                        <img class="media-object img-rounded" style="width: 140px; height: 140px;" src="img/altigran.jpg" alt="140x140">
-                    </a>
-                </div>
-            <div class="col-md-5">
-                <h2 class="media-heading">Nome do professor</h2>
-                <p>
-                    Formação: <br>
-                    Local: <br>
-                    Ano: <br>
-                    Lattes: <a class=\"lattes\" href=\"http://lattes.cnpq.br/lattes\" target=\"_blank\">http://lattes.cnpq.br/Lattes</a><br>
-                    E-mail: <a id="endereco" class="envelope lattes"><span class="glyphicon glyphicon glyphicon-envelope" aria-hidden="true"></span></a><img src="img/email-altigran.png">
-                    <?php
-                        $image = imagecreatetruecolor(250, 25);
-                        $background_color = imagecolorallocate($image, 224, 242, 241);  
-                        imagefilledrectangle($image,0,0,250,25,$background_color);
-                        $textcolor = imagecolorallocate($image, 0, 0, 0);
-                        $font = "font-awesome/css/helveticaneue-light.ttf";
-                        imagettftext($image, 13, 0, 5, 16, $textcolor, $font, 'alti@icomp.ufam.edu.br');
-                        imagepng($image, "img/emailaltigran.png");
-                        
-                        $email = explode("@", 'alti@icomp.ufam.edu.br');
-                    ?>
-                </p>
-            </div>
-            <div class="col-xs-6 col-md-3">
-                <a href="http://icomp.ufam.edu.br">
-                    <img class="img-responsive" style="width: 250px; height: 140px;" src="img/icomp.png" alt="250x140">
-                </a>
-            </div>
-            <div class="col-xs-4 col-md-2">
-                <a href="http://www.ufam.edu.br">
-                    <img class="img-responsive" style="width: 120px; height: 140px;" src="img/ufam.png" alt="120x140">
-                </a>
-            </div>
-        </div>
-        <hr>
-        <p class="text-justify">Descrição</p>
-        <!-- </div> -->
-    </div> 
-    
-    <!-- VIEWS AQUI! -->
-    <?= $content ?>
+            <!-- VIEWS AQUI! -->
+            <?= $content ?>
+        </div> 
+    </div>
 
     <!-- footer -->
     <div class="container">
@@ -147,46 +106,7 @@ AppAsset::register($this);
     <script src="js/docs.min.js"></script>
     <script src="js/ie10-viewport-bug-workaround.js"></script>
 
-    <script type="text/javascript">
-        $(document).on('click', '.panel-heading span.clickable', function (e) {
-            var $this = $(this);
-            if (!$this.hasClass('panel-collapsed')) {
-                $this.parents('.panel').find('.panel-body').slideUp();
-                $this.addClass('panel-collapsed');
-                $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-            } else {
-                $this.parents('.panel').find('.panel-body').slideDown();
-                $this.removeClass('panel-collapsed');
-                $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-            }
-        });
-        $(document).on('click', '.panel div.clickable', function (e) {
-            var $this = $(this);
-            if (!$this.hasClass('panel-collapsed')) {
-                $this.parents('.panel').find('.panel-body').slideUp();
-                $this.addClass('panel-collapsed');
-                $this.find('i').removeClass('glyphicon-minus').addClass('glyphicon-plus');
-            } else {
-                $this.parents('.panel').find('.panel-body').slideDown();
-                $this.removeClass('panel-collapsed');
-                $this.find('i').removeClass('glyphicon-plus').addClass('glyphicon-minus');
-            }
-        });
-        $(document).ready(function () {
-            $('.panel-heading span.clickable').click();
-            $('.panel div.clickable').click();
-        });
-    </script>
-
-    <script type="text/javascript">
-        dominio = "alti@icomp.ufam.edu.br";
-        arroba = "@";
-        mt = "mailto:";
-        endereco = document.getElementById("endereco");
-        endereco.setAttribute("href", mt + "<?= $email[0]?>" + arroba + dominio);
-    </script>
-
-<?php $this->endBody() ?>
+    <?php $this->endBody() ?>
 </body>
 <?php $this->endPage() ?>
 </html>
